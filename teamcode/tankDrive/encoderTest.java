5ada1c362fa13a891c71d7862f0cfd077e11570b
Neodog <108339770+TheLaziestDog@users.noreply.github.com>
2024-09-10 16:41:06 +0700

    teamcode

---
+++ teamcode/tankDrive/encoderTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.tankDrive;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp
+public class encoderTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Find a motor in the hardware map named "Arm Motor"
+        DcMotor motor = hardwareMap.dcMotor.get("leftDrive");
+
+        // Reset the motor encoder so that it reads zero ticks
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // Turn the motor back on, required if you use STOP_AND_RESET_ENCODER
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double CPR = 292.12;
+
+            // Get the current position of the motor
+            int position = motor.getCurrentPosition();
+            double revolutions = position/CPR;
+
+            double angle = revolutions * 360;
+            double angleNormalized = angle % 360;
+
+            // Show the position of the motor on telemetry
+            telemetry.addData("Encoder Position", position);
+            telemetry.addData("Encoder Revolutions", revolutions);
+            telemetry.addData("Encoder Angle (Degrees)", angle);
+            telemetry.addData("Encoder Angle - Normalized (Degrees)", angleNormalized);
+            telemetry.update();
+        }
+    }
+}
